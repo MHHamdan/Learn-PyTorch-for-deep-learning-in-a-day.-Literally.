aa79eb82691b48f6e20de1aeedfe91f61eb612cd
Mohammed Hamdan <43042738+MHHamdan@users.noreply.github.com>
2022-09-05 19:24:16 -0400

    Add files via upload

---
+++ ViT_replicate.py
@@ -0,0 +1,28 @@
+""":cvar
+A machine learning paper is a scientific paper that details findings of a research group on a specific area.
+The contents of a machine learning reasearch paper can vary from paper to paper but they generally follow the structure:
+
+Section/Contents:
+Abstract: An overview/summary of the paper's main findings/contributions.
+Introduction: What's the paper's main problem and details of previous methods used to try and solve it.
+ Method: How did the reseachers go about conductiong thier research? e.g. what model(s), data source(s), training setups, and
+ were used...
+ Results: What are the outcomes of the paper? If a new type of model or training setup was used, how did the results is
+ of findings compare to previous works? (this is where experiment tracing comes in handy)
+ Conclusions: WHat are teh limitations of the suggested methods? WHat are some next steps for the reserch community?
+ Reference: What resources/other papers did the reseachers look at to build their own body of work?
+ Appendix: Are there any extra resources/findings to look a t that weren't included in any of the above sections?
+
+"""
+
+print(f"Why replicating a machine learning paper? ::\n"
+      f"\nA machine learning reserch paper is often a presentation of months of work and experiments done by some of the best work"
+      f"\nmachine learning teams in thw world condensed into a few pages of text..."
+      f"\nAnd if these experiments lead to better results in an area related to the probelm you're working on,"
+      f"it'd be nice to them out... \nAlso, replicating the work of others is a fantastic way to practice your skills.. "
+      f"\n \n \n Machine learning engineer: "
+      f"\n1. Download a paper "
+      f"\n2. Implement it.."
+      f"\n3. Keep doing this until you have skills .."
+      f"\n by George Hots, a comm.ai founder,  a self-driving car company an deliverstreams maching  learning coding "
+      f"\n on Twitch and those videos get posted in full to Youtube .. ")
